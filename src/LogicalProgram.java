acc0797354c5856f8abc4056cd0019ae9aff07cc
baselvam <baselvam@paypal.com>
2022-03-16 10:10:48 +0530

    Added the logical program for printPairOfSums

--- src/LogicalProgram.java
+++ src/LogicalProgram.java
@@ -2,16 +2,28 @@ public class LogicalProgram {
 
     static char[] characters = {'a', 'b', 'c', 'd'};
 
+    static int[] num = {1, 2, 3, 4};
+
     public static void main(String[] args) {
-        printPairOfArrays(characters);
+        printPairOfSums(num);
     }
 
-    public static void printPairOfArrays(char[] num) {
-        for (int i = 0; i < num.length; i++) {
-            for (int j = 0; j < num.length; j++) {
+    public static void printPairOfArrays(char[] characters) {
+        for (int i = 0; i < characters.length; i++) {
+            for (int j = 0; j < characters.length; j++) {
                 if (i != j) {
-                    System.out.println(num[i] + "," + num[j]);
+                    System.out.println(characters[i] + "," + characters[j]);
                 }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printPairOfSums(int[] num) {
+        for (int k : num) {
+            for (int i : num) {
+                int sum = k + i;
+                System.out.print(sum + ", ");
             }
             System.out.println();
         }
