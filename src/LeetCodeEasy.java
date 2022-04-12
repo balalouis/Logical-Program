dd4295dec4830b0227cbfd28e2e13f6561c95fab
balan.ssn <balan.ssn@gmail.com>
2022-04-12 08:29:31 +0530

    Code clean up

---
+++ src/LeetCodeEasy.java
@@ -0,0 +1,16 @@
+
+public class LeetCodeEasy {
+    public boolean isPalindrome(int x) {
+        char ss[] = String.valueOf(x).toCharArray();
+        boolean isPalindrome = true;
+        for (int front = 0, back = ss.length - 1; front < ss.length / 2; front++, back--) {
+            if (ss[front] != ss[back]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        return isPalindrome;
+    }
+
+}
+
