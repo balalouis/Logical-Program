74ac1bd0a0cb25a91d459fd9bbbdde868341370e
Balamurugan Selvam <balan.job@gmail.com>
2023-09-27 22:38:58 +0530

    Logic for pivot program

--- src/leetcode/LeetCodeMediumKotlin.kt
+++ src/leetcode/LeetCodeMediumKotlin.kt
@@ -3,12 +3,36 @@ package leetcode
 class LeetCodeMediumKotlin {
 
     fun pivotArray(nums: IntArray, pivot: Int): IntArray {
-
+        val finalArray = IntArray(nums.size)
+        var finalPos = 0
+        val firstHalf = IntArray(nums.size)
+        var firstPos = 0
+        val secondHalf = IntArray(nums.size)
+        var secondPos = 0
+        var sameNumCount = 0
         for (n in nums) {
-            System.out.println("Number: $n")
+            if (n < pivot) {
+                firstHalf[firstPos++] = n
+                finalArray[finalPos++] = n
+            } else if (n > pivot) {
+                secondHalf[secondPos++] = n
+            } else {
+                sameNumCount++
+            }
         }
 
-        return nums
+        while (sameNumCount != 0) {
+            finalArray[finalPos++] = pivot
+            sameNumCount--
+        }
+
+        for (s in secondHalf) {
+            if (s != 0) {
+                finalArray[finalPos++] = s
+            }
+        }
+        println(finalArray.contentToString())
+        return finalArray
     }
 
 }
